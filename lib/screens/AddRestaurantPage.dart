361729dc0f732d72c4c917cdcc39614ec985d284
malek <maleksouissi751@gmail.com>
2021-03-25 11:03:29 +0100

    123

--- lib/screens/AddRestaurantPage.dart
+++ lib/screens/AddRestaurantPage.dart
@@ -153,7 +153,7 @@ class AddRestaurant extends StatelessWidget {
                           MaterialPageRoute(
                               builder: (context) => HomePage()));
                     },
-                    title: 'Add Restaurant',
+                    title: 'Add Restauran',
                     bcolor: KBeige,
                     size: 25,
                     color: Color(0xFF333133),
